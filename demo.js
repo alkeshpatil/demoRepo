6954855ff734bfc4dee5c28297697228da17689a
alkesh <alkesh@patil.com>
2021-02-01 17:58:22 -1200

    enter2

--- demo.js
+++ demo.js
@@ -2,8 +2,8 @@ const http = require('http');
 const fs =require('fs')
 const fileContent = fs.readFileSync('index.html')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = 'localhost';
+const port = 5000;
 
 const server = http.createServer((req, res) => {
  res.writeHead(200 , {'Content-type':'text/html'});
